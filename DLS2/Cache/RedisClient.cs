06bc2d14d2728a7126d2461fc7eaeb16139b4c13
Andylaa10 <andylaa100@gmail.com>
2024-04-16 12:25:13 +0200

    setup caching with redis

---
+++ DLS2/Cache/RedisClient.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+using StackExchange.Redis;
+
+namespace Cache;
+
+public class RedisClient
+{
+    private readonly string _hostname;
+    private readonly int _port;
+    private readonly string _password;
+
+    private ConnectionMultiplexer redis;
+
+    public RedisClient(string hostname, int port, string password)
+    {
+        _hostname = hostname;
+        _port = port;
+        _password = password;
+    }
+
+    public void Connect()
+    {
+        string connectionString = $"{_hostname}:{_port},password={_password}";
+        redis = ConnectionMultiplexer.Connect(connectionString);
+    }
+
+    public IDatabase GetDatabase()
+    {
+        return redis.GetDatabase();
+    }
+
+    public void StoreValue(string key, string value)
+    {
+        var db = GetDatabase();
+        db.StringSet(key, value);
+    }
+
+    public string? GetValue(string key)
+    {
+        var db = GetDatabase();
+        return db.StringGet(key);
+    }
+
+    public void RemoveValue(string key)
+    {
+        var db = GetDatabase();
+        db.KeyDelete(key);
+    }
+
+    public string SerializeObject<T>(T obj)
+    {
+        return JsonSerializer.Serialize(obj);
+    }
+
+    public T? DeserializeObject<T>(string json)
+    {
+        return JsonSerializer.Deserialize<T>(json);
+    }
+}
