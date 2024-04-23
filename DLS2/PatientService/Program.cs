0165c2d990338d530f68234ab0866a4e587b7c42
KristianHollaender <90904724+KristianHollaender@users.noreply.github.com>
2024-04-23 10:43:53 +0200

    Redid CORS in program.cs, maybe it works

--- DLS2/PatientService/Program.cs
+++ DLS2/PatientService/Program.cs
@@ -3,21 +3,13 @@ using PatientService.Configs;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.ConfigureDependencyInjection();
+builder.Services.AddCors();
 
 var app = builder.Build();
-
-app.UseCors(options =>
-{
-    options
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowAnyOrigin();
-});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -25,6 +17,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(options =>
+{
+    options.SetIsOriginAllowed(origin => true)
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+});
 
 app.UseAuthorization();
 
